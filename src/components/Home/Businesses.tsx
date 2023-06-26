092479b6a9ed6e85f437be89e7e4f93554f2346b
Charles Kamiri <charleskamiri6@gmail.com>
2023-06-27 02:56:49 +0300

    UI updates

--- src/components/Home/Businesses.tsx
+++ src/components/Home/Businesses.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React from 'react';
+import './Businesses.scss';
 
 const Businesses: React.FC = () => {
   return (
-    <div className='businesses'>This is the businesses page. All your businesses will show up here!</div>
+    <div className='businesses'>
+      <div className="business-card">
+          <div className="business-img">
+            <i className="bx bxs-image-alt"></i>
+          </div>
+
+          <div className="business-meta">
+
+            <div className="name">
+              <i className="bx bx-buildings"></i>
+              <span>business_name</span>
+            </div>
+
+            <div className="email">
+              <i className="bx bx-envelope"></i>
+              <span>abc123@gmail.com</span>
+            </div>
+
+            <div className="location">
+              <i className="bx bx-map-pin"></i>
+              <span>Ngara, Nairobi</span>
+            </div>
+
+            <div className="buttons">
+              <i className="bx bx-edit"></i>
+              <i className="bx bx-cog"></i>
+              <i className="bx bx-dots-vertical-rounded"></i>
+            </div>
+          </div>
+      </div>
+    </div>
   )
 }
 
