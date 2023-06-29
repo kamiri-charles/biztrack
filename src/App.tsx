7b3034e8defbfb20e1c3ec748600fdd412144c69
Charles Kamiri <charleskamiri6@gmail.com>
2023-06-29 13:13:13 +0300

    Switched to hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {useRef, useEffect} from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {HashRouter as Router, Routes, Route} from 'react-router-dom';
 import Welcome from './components/Welcome';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
